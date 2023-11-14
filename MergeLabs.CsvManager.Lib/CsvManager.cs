57a65f1d16fd1bc0766a64bca92f783316ee6683
Luís Inácio <lrs.inacio@gmail.com>
2023-11-14 15:19:58 +0000

    Change in structure, added given code for csvmanager and tests

---
+++ MergeLabs.CsvManager.Lib/CsvManager.cs
@@ -0,0 +1,18 @@
+﻿namespace MergeLabs.CsvManager.Lib;
+
+public class CsvManager
+{
+    private const char CsvNewLineCharacter = '\n';
+    private const char CsvCellSeparator = ','; // seperator typo
+    private const string CsvNullCellIdentifier = "NULL";
+
+    public static string SimpleTransformCsv(string S)
+    {
+        return string.Empty;
+    }
+
+    public static string TransformCsv(string S)
+    {
+        return string.Empty;
+    }
+}
