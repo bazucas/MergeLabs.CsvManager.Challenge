2c501362cd0931d7df58375eabc5b67070bd9251
Luís Inácio <lrs.inacio@gmail.com>
2023-11-14 16:36:44 +0000

    Update DAL class with method to retrieve mock csv for benchmark, and benchmark methods added

---
+++ MergeLabs.CsvManager.Benchmark/Benchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace MergeLabs.CsvManager.Benchmark;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class Benchmark
+{
+    private string _csvData = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var dataAccess = new DataAccess.CsvMockDataProvider();
+        _csvData = dataAccess.RetrieveCsvData();
+    }
+
+    [Benchmark]
+    public void Benchmark_SimpleTransformCsv()
+    {
+        Lib.CsvManager.SimpleTransformCsv(_csvData);
+    }
+
+    [Benchmark]
+    public void Benchmark_TransformCsv()
+    {
+        Lib.CsvManager.TransformCsv(_csvData);
+    }
+}
---
+++ MergeLabs.CsvManager.DataAccess/CsvMockDataProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace MergeLabs.CsvManager.DataAccess;
+
+public class CsvMockDataProvider
+{
+    // could retrieve this from local or azure appSettings or through key vault as well
+    private const string CsvFilePath = "./CsvFile/mockdata.csv";
+
+    /// <summary>
+    /// Reads and returns the content of a CSV file as a single string.
+    /// </summary>
+    /// <remarks>
+    /// This method checks if the specified CSV file exists at the defined path.
+    /// If the file does not exist, it returns an empty string. Otherwise, it reads the entire content
+    /// of the file and returns it as a string, preserving the original line breaks of the file.
+    /// </remarks>
+    /// <returns>
+    /// A string containing the entire content of the CSV file if the file exists, or an empty string if the file does not exist.
+    /// </returns>
+    public string RetrieveCsvData() =>
+        !File.Exists(CsvFilePath) ? string.Empty : File.ReadAllText(CsvFilePath);
+}
